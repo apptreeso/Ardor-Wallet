649a4f7525232f9b76366ad798cac1c7a4ca9699
buckdy <buck3dy@gmail.com>
2021-08-09 18:50:03 -0300

    [LD-303]: implements ERC721

--- contracts/CorePoolV2.sol
+++ contracts/CorePoolV2.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
 
-contract CorePoolV2 {
-
-}
+contract CorePoolV2 is ERC721 {}
