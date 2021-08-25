ff1c914c165933f9319b4c38ce5dcdc67e14c45b
buckdy <buck3dy@gmail.com>
2021-08-25 16:25:14 -0300

    [LD-311]: fix lib import

--- contracts/interfaces/IPoolBase.sol
+++ contracts/interfaces/IPoolBase.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
+import { Stake } from "../libraries/Stake.sol";
+
 interface IPoolBase {
     /**
      * @dev Deposit is a key data structure used in staking,
