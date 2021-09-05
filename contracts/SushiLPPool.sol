a6c1ec4f4f6cc2d1edfc8d4ec7e49eacde6ab899
buckdy <buck3dy@gmail.com>
2021-09-05 17:00:52 -0300

    [LD-321]: sushi lp pool contract

--- contracts/SushiLPPool.sol
+++ contracts/SushiLPPool.sol
@@ -3,4 +3,16 @@ pragma solidity 0.8.4;
 
 import { CorePool } from "./base/CorePool.sol";
 
-contract SushiLPPool {}
+contract SushiLPPool is CorePool {
+    /// @dev see __CorePool_init()
+    function __SushiLPPool_init(
+        address _ilv,
+        address _silv,
+        address _poolToken,
+        address _factory,
+        uint64 _initTime,
+        uint32 _weight
+    ) external initializer {
+        __CorePool_init(_ilv, _silv, _poolToken, _factory, _initTime, _weight);
+    }
+}
