f7ac2f691507805162eb7eca3b25a9a4f4df2b98
buckdy <buck3dy@gmail.com>
2021-09-20 17:08:04 -0300

    [LD-329]: adds bigger stake test scenario

--- test/CorePool.behavior.ts
+++ test/CorePool.behavior.ts
@@ -84,6 +84,99 @@ export function pendingYield(usingPool?: string): () => void {
       expect(Number(aliceYield)).to.be.equal(expectedRewards / 2);
       expect(Number(bobYield)).to.be.equal(expectedRewards / 2);
     });
+    it("should calculate pending rewards correctly after bigger stakes", async function () {
+      const token = getToken(this.ilv, this.lp, usingPool as string);
+      const pool = getPool(this.ilvPool, this.lpPool, usingPool as string);
+
+      const poolWeight = await pool.weight();
+      const totalWeight = await this.factory.totalWeight();
+
+      await token.connect(this.signers.alice).approve(pool.address, MaxUint256);
+      await pool.connect(this.signers.alice).stakeFlexible(toWei(10));
+
+      await pool.setNow256(INIT_TIME + 50);
+
+      const { pendingYield: aliceYield0 } = await pool.pendingRewards(this.signers.alice.address);
+
+      const expectedAliceYield0 = ILV_PER_SECOND.mul(50).mul(poolWeight).div(totalWeight);
+
+      await token.connect(this.signers.bob).approve(pool.address, MaxUint256);
+      await pool.connect(this.signers.bob).stakeAndLock(toWei(5000), ONE_YEAR * 2);
+
+      const totalInPool = toWei(10 * 1e6).add(toWei(5000 * 2e6));
+
+      const { pendingYield: bobYield0 } = await pool.pendingRewards(this.signers.bob.address);
+
+      const expectedBobYield0 = 0;
+
+      await pool.setNow256(INIT_TIME + 200);
+
+      const { pendingYield: aliceYield1 } = await pool.pendingRewards(this.signers.alice.address);
+      const { pendingYield: bobYield1 } = await pool.pendingRewards(this.signers.bob.address);
+
+      const expectedAliceYield1 = Number(
+        ethers.utils.formatEther(
+          ILV_PER_SECOND.mul(150)
+            .mul(toWei(10 * 1e6))
+            .div(totalInPool)
+            .mul(poolWeight)
+            .div(totalWeight)
+            .add(expectedAliceYield0),
+        ),
+      ).toFixed(3);
+
+      const expectedBobYield1 = Number(
+        ethers.utils.formatEther(
+          ILV_PER_SECOND.mul(150)
+            .mul(toWei(5000 * 2e6))
+            .div(totalInPool)
+            .mul(poolWeight)
+            .div(totalWeight),
+        ),
+      ).toFixed(3);
+
+      expect(expectedAliceYield0).to.be.equal(aliceYield0);
+      expect(expectedAliceYield1).to.be.equal(Number(ethers.utils.formatEther(aliceYield1)).toFixed(3));
+      expect(expectedBobYield0).to.be.equal(bobYield0);
+      expect(expectedBobYield1).to.be.equal(Number(ethers.utils.formatEther(bobYield1)).toFixed(3));
+    });
+    // it("should not accumulate yield after yield farming ends", async () => {
+    //   await ilv.approve(ilvPool.address, MAX_UINT256, { from: bob });
+    //   await ilv.approve(ilvPool.address, MAX_UINT256, { from: carol });
+
+    //   await ilvPool.stake(500, 0, false, { from: bob });
+    //   await ilvPool.stake(500, 0, false, { from: carol });
+
+    //   await ilvPool.setBlockNumber(1010);
+
+    //   const pendingYield0 = Number(await ilvPool.pendingYieldRewards(carol));
+    //   const expectedYield0 = 10000 / 2;
+
+    //   await ilvPool.setBlockNumber(7117500);
+
+    //   const pendingYield1 = Number(await ilvPool.pendingYieldRewards(carol));
+    //   // calculates based on total ILV emitted / 2 (since we have 2 stakers with same weights)
+    //   const expectedYield1 = ((7117500 - 10) * 10) / 2;
+
+    //   await ilvPool.setBlockNumber(99999999999);
+
+    //   const pendingYieldFinal0 = Number(await ilvPool.pendingYieldRewards(carol));
+
+    //   await ilvPool.sync();
+
+    //   const pendingYieldFinal1 = Number(await ilvPool.pendingYieldRewards(carol));
+
+    //   await ilvPool.processRewards(false, { from: carol });
+    //   const balance = Number(await ilvPool.balanceOf(carol));
+    //   // expected yield + original stake
+    //   const expectedBalance = expectedYield1 + 500;
+
+    //   expect(pendingYield0).to.equal(expectedYield0);
+    //   expect(pendingYield1).to.equal(expectedYield1);
+    //   expect(pendingYieldFinal0).to.equal(expectedYield1);
+    //   expect(pendingYieldFinal1).to.equal(expectedYield1);
+    //   expect(balance).to.equal(expectedBalance);
+    // });
   };
 }
 
