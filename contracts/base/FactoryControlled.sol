7f9e5c82efc1b1f81d5e3f8c1ff3df160fb974a7
buckdy <buck3dy@gmail.com>
2021-10-08 16:58:35 -0300

    [LD-362]: fixes factory controller error

--- contracts/base/FactoryControlled.sol
+++ contracts/base/FactoryControlled.sol
@@ -9,11 +9,7 @@ abstract contract FactoryControlled is Initializable {
     IFactory public factory;
 
     function __FactoryControlled_init(address _factory) internal initializer {
-        // verify PoolFactory instance supplied
-        require(
-            IFactory(_factory).FACTORY_UID() == 0xc5cfd88c6e4d7e5c8a03c255f03af23c0918d8e82cac196f57466af3fd4a5ec7,
-            "unexpected FACTORY_UID"
-        );
+        require(_factory != address(0));
 
         factory = IFactory(_factory);
     }
