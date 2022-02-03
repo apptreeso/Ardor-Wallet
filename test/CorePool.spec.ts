2b1df3cead031f9aa0ca7c36c17ce16ae1bb5172
buckdy <buck3dy@gmail.com>
2022-02-03 18:06:45 -0300

    add missing tests

--- test/CorePool.spec.ts
+++ test/CorePool.spec.ts
@@ -129,12 +129,12 @@ describe("CorePools", function () {
     await this.lp.connect(this.signers.deployer).transfer(await toAddress(this.signers.bob), toWei(10000));
     await this.lp.connect(this.signers.deployer).transfer(await toAddress(this.signers.carol), toWei(10000));
   });
-  // describe("Upgrades", upgradePools());
-  // describe("#setEndTime", setEndTime());
-  // describe("#getPoolData", function () {
-  //   context("ILV Pool", getPoolData("ILV"));
-  //   context("Sushi LP Pool", getPoolData("LP"));
-  // });
+  describe("Upgrades", upgradePools());
+  describe("#setEndTime", setEndTime());
+  describe("#getPoolData", function () {
+    context("ILV Pool", getPoolData("ILV"));
+    context("Sushi LP Pool", getPoolData("LP"));
+  });
   describe("#blacklistUser", function () {
     context("ILV Pool", blacklistUsers("ILV"));
     context("Sushi LP Pool", blacklistUsers("LP"));
